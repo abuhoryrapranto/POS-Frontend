aad34b7405fbb6e00c116243ff8bd285de021359
mahpranto <mahpranto@gmail.com>
2021-07-12 13:53:44 +0600

    Intial Commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,28 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6">
+
+      </div>
+      <div class="col-md-6 login-side">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+.login-side {
+  height: 100vh;
+  background: linear-gradient(to top, #33ccff 0%, #3366ff 100%);
+}
+
+</style>
